3f60550d23e7c369374f653d6696881bdd131d48
heydaripoor <m.heydaripoor@gmail.com>
2019-09-03 20:56:31 +0430

    ایجاد کلید چاپ

--- web/src/main/java/com/nicico/training/controller/CommitteeFormController.java
+++ web/src/main/java/com/nicico/training/controller/CommitteeFormController.java
@@ -1,8 +1,17 @@
 package com.nicico.training.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,4 +22,66 @@ public class CommitteeFormController {
 	public String showForm() {
 		return "base/committee";
 	}
+	
+	
+//	@PostMapping("/printWithCriteria/{type}/{committeeId}")
+//	public ResponseEntity<?> printWithCriteria(final HttpServletRequest request, @PathVariable String type) {
+//		String token = (String) request.getSession().getAttribute("AccessToken");
+//
+//		final RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+//
+//		final HttpHeaders headers = new HttpHeaders();
+//		headers.add("Authorization", "Bearer " + token);
+//
+//		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//
+//		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+//		map.add("CriteriaStr", request.getParameter("CriteriaStr"));
+//
+//		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+//
+//		String restApiUrl = request.getRequestURL().toString().replace(request.getServletPath(),"");
+//
+//		if(type.equals("pdf"))
+//			return restTemplate.exchange(restApiUrl + "/api/committee/printWithCriteria/PDF", HttpMethod.POST, entity, byte[].class);
+//		else if(type.equals("excel"))
+//			return restTemplate.exchange(restApiUrl + "/api/committee/printWithCriteria/EXCEL", HttpMethod.POST, entity, byte[].class);
+//		else if(type.equals("html"))
+//			return restTemplate.exchange(restApiUrl + "/api/committee/printWithCriteria/HTML", HttpMethod.POST, entity, byte[].class);
+//		else
+//			return null;
+//	}
+//
+
+
+@RequestMapping("/printCommitteeWithMember/{type}")
+	public ResponseEntity<?> printAll(final HttpServletRequest request, @PathVariable String type) {
+		String token = (String) request.getSession().getAttribute("AccessToken");
+
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+
+		final HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + token);
+
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		String restApiUrl = request.getRequestURL().toString().replace(request.getServletPath(),"");
+
+
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		if(type.equals("pdf"))
+			return restTemplate.exchange(restApiUrl + "/api/committee/printCommitteeWithMember/pdf", HttpMethod.GET, entity, byte[].class);
+		else if(type.equals("excel"))
+			return restTemplate.exchange(restApiUrl + "/api/committee/printCommitteeWithMember/excel", HttpMethod.GET, entity, byte[].class);
+		else if(type.equals("html"))
+			return restTemplate.exchange(restApiUrl + "/api/committee/printCommitteeWithMember/html", HttpMethod.GET, entity, byte[].class);
+		else
+			return null;
+	}
+
+
+	
 }
