126ac59a81bde25851d56b5ebde47541c61fb1c6
ghazanfari_f <ghazanfari_f@nicico.com>
2019-09-03 09:33:43 +0430

    Job

--- service/src/main/java/com/nicico/training/dto/JobDTO.java
+++ service/src/main/java/com/nicico/training/dto/JobDTO.java
@@ -3,10 +3,7 @@ ghazanfari_f, 8/29/2019, 10:48 AM
 */
 package com.nicico.training.dto;
 
-import com.nicico.training.model.enums.EDeleted;
-import com.nicico.training.model.enums.EEnabled;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -26,7 +23,5 @@ public class JobDTO implements Serializable {
         private Long id;
         private String code;
         private String titleFa;
-        private EEnabled eEnabled;
-        private EDeleted eDeleted;
     }
 }
