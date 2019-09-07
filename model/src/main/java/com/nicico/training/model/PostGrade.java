d740946844bfbdfcf332bfe5774bd00a3f9007d7
ghazanfari_f <ghazanfari_f@nicico.com>
2019-09-07 10:25:45 +0430

    Post

--- model/src/main/java/com/nicico/training/model/PostGrade.java
+++ model/src/main/java/com/nicico/training/model/PostGrade.java
@@ -13,10 +13,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "tbl_post_grade")
 @Getter
 @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tbl_post_grade")
 public class PostGrade {
 
     @Id
