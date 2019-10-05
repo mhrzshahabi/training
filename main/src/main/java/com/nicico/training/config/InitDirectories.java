f59f19a3d6d3380cb9b762415e90718e75325187
public <a@a.com>
2019-10-05 11:09:29 +0330

    Init Directories

---
+++ main/src/main/java/com/nicico/training/config/InitDirectories.java
@@ -0,0 +1,49 @@
+/*
+ghazanfari_f, 9/30/2019, 9:29 AM
+*/
+package com.nicico.training.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Component
+public class InitDirectories {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Value("${nicico.dir.upload.person.img}")
+    private String personImgDir;
+
+    @Value("${nicico.dir.upload.person.tmp}")
+    private String personImgTmpDir;
+
+    @Value("${nicico.dir.upload.bpmn}")
+    private String bpmnDir;
+
+    @PostConstruct
+    private void init() {
+        try {
+            createDir(personImgDir);
+            createDir(personImgTmpDir);
+            createDir(bpmnDir);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void createDir(String path) {
+        try {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
