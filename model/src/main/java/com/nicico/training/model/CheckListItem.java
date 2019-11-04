97cfb4079d58d930fb08c1d0ee97f7b2e6998d00
heydaripoor <m.heydaripoor@gmail.com>
2019-11-04 20:18:42 +0330

    Create checkLIstDAO

---
+++ model/src/main/java/com/nicico/training/model/CheckListItem.java
@@ -0,0 +1,29 @@
+package com.nicico.training.model;
+
+import javax.persistence.*;
+
+public class CheckListItem extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Check_List_Item_seq")
+    @SequenceGenerator(name = "Check_List_Item_seq", sequenceName = "seq_Check_List_Item_id", allocationSize = 1)
+    @Column(name = "id", precision = 10)
+    private Long id;
+
+    @Column(name = "c_title_fa")
+    private String titleFa;
+
+    @Column(name = "c_description")
+    private String description;
+
+    @Column(name = "b_enabled")
+    private Boolean enableStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "f_check_list_id", insertable = false, updatable = false)
+    private CheckList checkList;
+
+    @Column(name = "f_check_list_id")
+    private Long CheckListId;
+
+
+}
