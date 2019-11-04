4fe041b8a64dcb44142bf2d458c047cfcaa07fb8
poursalehi_mos <poursalehi_mos@nicico.com>
2019-11-04 10:41:06 +0330

    add description to attachment entity

---
+++ service/src/main/java/com/nicico/training/dto/AttachmentDTO.java
@@ -0,0 +1,81 @@
+package com.nicico.training.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AttachmentDTO {
+
+    private String entityName;
+    private Long objectId;
+    private String fileName;
+    private String fileType;
+    private String description;
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("Attachment")
+    public static class Info extends AttachmentDTO {
+        private Long id;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("AttachmentCreateRq")
+    public static class Create extends AttachmentDTO {
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("AttachmentUpdateRq")
+    public static class Update extends AttachmentDTO {
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("AttachmentDeleteRq")
+    public static class Delete {
+        @NotNull
+        @ApiModelProperty(required = true)
+        private List<Long> ids;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @ApiModel("AttachmentSpecRs")
+    public static class AccountInfoSpecRs {
+        private SpecRs response;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class SpecRs {
+        private List<Info> data;
+        private Integer status;
+        private Integer startRow;
+        private Integer endRow;
+        private Integer totalRows;
+    }
+
+}
+
