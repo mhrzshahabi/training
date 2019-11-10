a38085ce218fcc45bebf8e81f40ce0fcabb2d3e0
poursalehi_mos <poursalehi_mos@nicico.com>
2019-11-10 11:41:30 +0330

    needAssessmentSkillToPost entity is created

--- model/src/main/java/com/nicico/training/model/NeedAssessmentSkillToPost.java
+++ model/src/main/java/com/nicico/training/model/NeedAssessmentSkillToPost.java
@@ -14,12 +14,12 @@ import javax.persistence.*;
 @Accessors(chain = true)
 @EqualsAndHashCode(of = "id", callSuper = false)
 @Entity
-@Table(name = "tbl_need_assessment_skill_based", uniqueConstraints = {@UniqueConstraint(columnNames = {"f_skill_id", "c_entity_name", "n_object_id"})})
-public class NeedAssessmentSkillBased extends Auditable {
+@Table(name = "tbl_need_assessment_skill_post", uniqueConstraints = {@UniqueConstraint(columnNames = {"f_skill_id", "f_post_id"})})
+public class NeedAssessmentSkillToPost extends Auditable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_need_assessment_skill_based_id")
-    @SequenceGenerator(name = "seq_need_assessment_skill_based_id", sequenceName = "seq_need_assessment_skill_based_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_need_assessment_skill_post_id")
+    @SequenceGenerator(name = "seq_need_assessment_skill_post_id", sequenceName = "seq_need_assessment_skill_post_id", allocationSize = 1)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -29,16 +29,17 @@ public class NeedAssessmentSkillBased extends Auditable {
     @Column(name = "f_skill_id", nullable = false)
     private Long skillId;
 
-    @Column(name = "c_entity_name", nullable = false)
-    private String entityName;
-
-    @Column(name = "n_object_id", nullable = false)
-    private Long objectId;
-
     @Column(name = "e_need_assessment_priority", insertable = false, updatable = false, nullable = false)
     private ENeedAssessmentPriority eNeedAssessmentPriority;
 
     @Column(name = "e_need_assessment_priority", nullable = false)
     private Integer eNeedAssessmentPriorityId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "f_post_id", insertable = false, updatable = false)
+    private Post post;
+
+    @Column(name = "f_post_id", nullable = false)
+    private Long postId;
 
 }
