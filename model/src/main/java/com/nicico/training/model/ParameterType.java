42b3ea5b763c5f6f42a8ba2eca778b5dff5c1490
ghazanfari_f <ghazanfari_f@nicico.com>
2019-12-07 09:53:07 +0330

    Parameter Type Class

---
+++ model/src/main/java/com/nicico/training/model/ParameterType.java
@@ -0,0 +1,25 @@
+package com.nicico.training.model;
+
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Accessors(chain = true)
+//@EqualsAndHashCode(of = "id")
+//@Entity
+//@Table(name = "tbl_parameter")
+public class ParameterType {
+//
+//    @Id
+//    @SequenceGenerator(name = "seq_parameter_id", )
+//    private long id;
+}
