a0570aa544d10e7a362ddd218c5d32f1dd4a114e
poursalehi_mos <poursalehi_mos@nicico.com>
2019-12-17 14:48:43 +0330

    Teacher Certification model

---
+++ model/src/main/java/com/nicico/training/model/TeacherCertification.java
@@ -0,0 +1,62 @@
+package com.nicico.training.model;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Entity
+@Table(name = "tbl_teacher_certification")
+public class TeacherCertification extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_certification_seq")
+    @SequenceGenerator(name = "teacher_certification_seq", sequenceName = "seq_teacher_certification_id", allocationSize = 1)
+    @Column(name = "id", precision = 10)
+    private Long id;
+
+    @Column(name = "c_course_title")
+    private String courseTitle;
+
+    @Column(name = "c_company_name")
+    private String companyName;
+
+    @Column(name = "c_company_location")
+    private String companyLocation;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "tbl_teacher_certification_category",
+            joinColumns = {@JoinColumn(name = "f_teacher_certification", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "f_category", referencedColumnName = "id")})
+    private List<Category> categories;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "tbl_teacher_certification_subcategory",
+            joinColumns = {@JoinColumn(name = "f_teacher_certification", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "f_subcategory", referencedColumnName = "id")})
+    private List<SubCategory> subCategories;
+
+    @Column(name = "n_duration", precision = 5)
+    private Integer duration;
+
+    @Column(name = "d_start_date")
+    private Date startDate;
+
+    @Column(name = "d_end_date")
+    private Date endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "f_teacher_id", insertable = false, updatable = false)
+    private Teacher teacher;
+
+    @Column(name = "f_teacher_id")
+    private Long teacherId;
+
+}
