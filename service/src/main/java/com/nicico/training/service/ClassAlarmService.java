6b8c474f56cc6f9436a3b0769844a44a58e4a18b
Rastegari_h <aaaa@aa>
2019-12-28 15:46:08 +0330

    attendance - alarm

--- service/src/main/java/com/nicico/training/service/ClassAlarmService.java
+++ service/src/main/java/com/nicico/training/service/ClassAlarmService.java
@@ -1,7 +1,9 @@
 package com.nicico.training.service;
 
+import com.nicico.copper.common.util.date.DateUtil;
 import com.nicico.training.dto.ClassAlarmDTO;
 import com.nicico.training.iservice.IClassAlarm;
+import jdk.internal.org.objectweb.asm.TypeReference;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -10,7 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,22 +28,28 @@ public class ClassAlarmService implements IClassAlarm {
 
 
     //*********************************
-      /*point : for ended classes do not fetch alarms*/
+    /*point : for ended classes do not fetch alarms*/
     @Transactional
     @Override
     public List<ClassAlarmDTO> list(Long class_id) {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String todayDate = DateUtil.convertMiToKh(dateFormat.format(date));
 
         List<Object> AlarmList = null;
         List<ClassAlarmDTO> classAlarmDTO = null;
         try {
 
-            String mainSql = "SELECT " +
+            StringBuilder alarmScript = new StringBuilder().append(" SELECT " +
                     "    f_class_id AS targetRecordId, " +
                     "    'classSessionsTab' AS tabName, " +
                     "    '/tclass/show-form' AS pageAddress, " +
                     "    'جلسات' AS alarmType, " +
                     "   (CASE WHEN floor( (class_time - session_time) / 60) > 0 THEN concat(concat('مجموع ساعات جلسات ',floor( (class_time - session_time) / 60) ),' ساعت کمتر از مدت کلاس است') " +
-                    "                            ELSE concat(concat('مجموع ساعات جلسات ',abs(floor( (class_time - session_time) / 60) ) ),' ساعت بیشتر از مدت کلاس است') END) AS alarm     " +
+                    "                            ELSE concat(concat('مجموع ساعات جلسات ',abs(floor( (class_time - session_time) / 60) ) ),' ساعت بیشتر از مدت کلاس است') END) AS alarm,     " +
+                    " 1 AS detailRecordId, " +
+                    " 'جلسات' AS sortField " +
                     " FROM " +
                     "    ( " +
                     "        SELECT " +
@@ -57,9 +68,36 @@ public class ClassAlarmService implements IClassAlarm {
                     "            tbl_class.c_title_class " +
                     "    ) " +
                     " WHERE " +
-                    "    floor(abs((class_time - session_time) / 60)) > 0";
+                    "    floor(abs((class_time - session_time) / 60)) > 0");
 
-            AlarmList = (List<Object>) entityManager.createNativeQuery(mainSql).setParameter("class_id",class_id).getResultList();
+            alarmScript.append(" UNION ALL ");
+
+            alarmScript.append(" SELECT DISTINCT targetRecordId, tabName, pageAddress, alarmType, alarm, detailRecordId, sortField " +
+                    " FROM " +
+                    " ( " +
+                    " SELECT tbl_session.f_class_id AS targetRecordId, " +
+                    "    'classAttendanceTab' AS tabName, " +
+                    "    '/tclass/show-form' AS pageAddress, " +
+                    "    'حضور و غیاب' AS alarmType, " +
+                    " ('حضور و غیاب ' || 'جلسه ' ||  tbl_session.c_session_start_hour || ' تا ' || tbl_session.c_session_end_hour || ' ' || tbl_session.c_day_name || ' تاریخ ' || tbl_session.c_session_date || ' تکمیل نشده است') as alarm, " +
+                    "    tbl_session.id AS detailRecordId, " +
+                    "    ('حضور و غیاب' || tbl_session.c_session_date || tbl_session.c_session_start_hour) AS sortField  " +
+                    " FROM " +
+                    "    tbl_class_student " +
+                    "    INNER JOIN tbl_session ON tbl_class_student.f_class = tbl_session.f_class_id " +
+                    "    LEFT JOIN tbl_attendance ON tbl_session.id = tbl_attendance.f_session " +
+                    " WHERE  " +
+                    "    tbl_session.f_class_id = :class_id  " +
+                    "    AND tbl_session.c_session_date <:todaydat " +
+                    "    AND (tbl_attendance.c_state IS NULL OR tbl_attendance.c_state = 0) " +
+                    " ) " +
+                    " ORDER BY sortField ");
+            //***order by must be in the last script***
+
+
+            AlarmList = (List<Object>) entityManager.createNativeQuery(alarmScript.toString())
+                    .setParameter("class_id", class_id)
+                    .setParameter("todaydat", todayDate).getResultList();
 
             if (AlarmList != null) {
                 classAlarmDTO = new ArrayList<>(AlarmList.size());
@@ -76,7 +114,8 @@ public class ClassAlarmService implements IClassAlarm {
             throw new RuntimeException("اشکال در ارتباط با دیتابیس");
         }
 
-        return (classAlarmDTO != null ? modelMapper.map(classAlarmDTO, new TypeToken<List<ClassAlarmDTO>>(){}.getType()) : null);
+        return (classAlarmDTO != null ? modelMapper.map(classAlarmDTO, new TypeToken<List<ClassAlarmDTO>>() {
+        }.getType()) : null);
 
     }
     //*********************************
