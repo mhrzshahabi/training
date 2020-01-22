c73833c6c12ae242839453973b8172195f967a0c
poursalehi_mos <poursalehi_mos@nicico.com>
2020-01-22 16:19:15 +0330

    get parameter value iscList by parameter code is added

--- service/src/main/java/com/nicico/training/service/ParameterService.java
+++ service/src/main/java/com/nicico/training/service/ParameterService.java
@@ -1,18 +1,24 @@
 package com.nicico.training.service;
 
 import com.nicico.copper.common.domain.criteria.SearchUtil;
+import com.nicico.copper.common.dto.grid.GridResponse;
+import com.nicico.copper.common.dto.grid.TotalResponse;
 import com.nicico.copper.common.dto.search.EOperator;
 import com.nicico.copper.common.dto.search.SearchDTO;
+import com.nicico.training.TrainingException;
 import com.nicico.training.dto.ParameterDTO;
 import com.nicico.training.dto.ParameterValueDTO;
 import com.nicico.training.model.Parameter;
 import com.nicico.training.repository.ParameterDAO;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +27,15 @@ public class ParameterService extends BaseService<Parameter, Long, ParameterDTO.
     @Autowired
     ParameterService(ParameterDAO parameterDAO) {
         super(new Parameter(), parameterDAO);
+    }
+
+    @Transactional(readOnly = true)
+    public TotalResponse<ParameterValueDTO.Info> getByCode(String code) {
+        Optional<Parameter> pByCode = dao.findByCode(code);
+        Parameter parameter = pByCode.orElseThrow(() -> new TrainingException(TrainingException.ErrorType.NotFound));
+        List<ParameterValueDTO.Info> infos = modelMapper.map(parameter.getParameterValueList(), new TypeToken<List<ParameterValueDTO.Info>>() {
+        }.getType());
+        return new TotalResponse<>(new GridResponse<>(infos));
     }
 
     //////////////////////////////////////////config//////////////////////////////////////////
