b5cab6f6eeb6a98bce398c573d8786fda34524ac
heydaripoor <m.heydaripoor@gmail.com>
2020-02-23 18:57:49 +0330

    create report

--- service/src/main/java/com/nicico/training/dto/unjustifiedAbsenceDTO.java
+++ service/src/main/java/com/nicico/training/dto/unjustifiedAbsenceDTO.java
@@ -1,23 +1,27 @@
 package com.nicico.training.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+
+import java.util.List;
+
 @Getter
 @Setter
 @Accessors(chain = true)
 @AllArgsConstructor
 public class unjustifiedAbsenceDTO {
-    private String session_date;
+    private String sessionDate;
     private String lname;
-    private String first_name;
-    private String title_class;
-    private String start_date;
-    private String end_date;
-    private String end_hour;
-    private String start_hour;
+    private String firstName;
+    private String titleClass;
+    private String startDate;
+    private String endDate;
+    private String endHour;
+    private String startHour;
 
     @Getter
     @Setter
@@ -31,8 +35,30 @@ public class unjustifiedAbsenceDTO {
         private String   lastName;
         private String   startDate;
         private String   endDate;
-       private String   personnelNo;
+       private String    personnelNo;
         private String   nationalCode;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("unjustifiedAbsenceSpecRs")
+    public static class unjustifiedAbsenceSpecRs {
+        private unjustifiedAbsenceDTO.SpecRs response;
+    }
+
+    //*********************************
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class SpecRs {
+        private List<unjustifiedAbsenceDTO.printScoreInfo> data;
+        private Integer status;
+        private Integer startRow;
+        private Integer endRow;
+        private Integer totalRows;
     }
 
    }
