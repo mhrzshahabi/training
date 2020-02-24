a616025e490ee130883a82787280271d73d8642f
hrastegari <hamed3213>
2020-02-24 08:30:57 +0330

    before push on the dev

--- repository/src/main/java/com/nicico/training/repository/NeedsAssessmentDAO.java
+++ repository/src/main/java/com/nicico/training/repository/NeedsAssessmentDAO.java
@@ -14,6 +14,10 @@ public interface NeedsAssessmentDAO extends BaseDAO<NeedsAssessment, Long> {
     @Modifying
     @Query(value = "update TBL_NEEDS_ASSESSMENT SET N_WORKFLOW_STATUS_CODE = :workflowStatusCode , C_WORKFLOW_STATUS = :workflowStatus WHERE f_object = (SELECT  f_object FROM TBL_NEEDS_ASSESSMENT WHERE id = :needsAssessmentId)", nativeQuery = true)
     public Integer updateNeedsAssessmentWorkflowStatus(Long needsAssessmentId, Integer workflowStatusCode, String workflowStatus);
+
+    @Query(value = "update TBL_NEEDS_ASSESSMENT SET N_MAIN_WORKFLOW_STATUS_CODE = :workflowStatusCode , C_MAIN_WORKFLOW_STATUS = :workflowStatus WHERE f_object = (SELECT  f_object FROM TBL_NEEDS_ASSESSMENT WHERE id = :needsAssessmentId)", nativeQuery = true)
+    public Integer updateNeedsAssessmentWorkflowMainStatus(Long needsAssessmentId, Integer workflowStatusCode, String workflowStatus);
+
 }
 
 
