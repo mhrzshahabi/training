bde6a4cfbf2d0ef1d372c4f285a8808bed9fdd04
matin_ar <amtin@nicico.com>
2020-06-09 11:55:53 +0430

    post grade group has been added

---
+++ service/src/main/java/com/nicico/training/service/ViewPostGradeGroupService.java
@@ -0,0 +1,18 @@
+package com.nicico.training.service;
+
+import com.nicico.training.dto.ViewPostGradeGroupDTO;
+import com.nicico.training.model.ViewPostGradeGroup;
+import com.nicico.training.repository.ViewPostGradeGroupDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ViewPostGradeGroupService extends BaseService<ViewPostGradeGroup, Long, ViewPostGradeGroupDTO.Info, ViewPostGradeGroupDTO.Info, ViewPostGradeGroupDTO.Info, ViewPostGradeGroupDTO.Info, ViewPostGradeGroupDAO>{
+    @Autowired
+    ViewPostGradeGroupService(ViewPostGradeGroupDAO viewPostGradeGroupDAO) {
+        super(new ViewPostGradeGroup(), viewPostGradeGroupDAO);
+    }
+
+}
