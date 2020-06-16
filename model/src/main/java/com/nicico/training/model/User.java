e083efdb3a1aeff09f62f947333ba87a36dabbd9
poursalehi_mos <poursalehi_mos@nicico.com>
2020-06-16 15:59:20 +0430

    Remove entities and rename sequences 2

---
+++ model/src/main/java/com/nicico/training/model/User.java
@@ -0,0 +1,30 @@
+package com.nicico.training.model;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Entity
+@Table(name = "tbl_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_user_id", allocationSize = 1)
+    @Column(name = "id", precision = 10)
+    private Long id;
+
+    @Column(name = "c_name")
+    private String name;
+
+    @Column(name = "c_lname")
+    private String Lname;
+
+
+}
