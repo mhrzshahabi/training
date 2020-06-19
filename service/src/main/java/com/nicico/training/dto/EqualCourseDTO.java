758ff61e66bec33081f165bc57a8328c8afa2a3d
Mostafa <poursalehi_mos@nicico.com>
2020-06-19 18:57:42 +0430

    course pre Course and equalCourse refactoring

---
+++ service/src/main/java/com/nicico/training/dto/EqualCourseDTO.java
@@ -0,0 +1,60 @@
+package com.nicico.training.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+
+public class EqualCourseDTO implements Serializable {
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("EqualCourse - Info")
+    public static class Info extends EqualCourseDTO {
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private Long id;
+        String idEC;
+        String nameEC;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("EqualCourse - Add")
+    public static class Add extends EqualCourseDTO {
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private Long id;
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private Long courseId;
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private List<Long> equalCoursesId;
+    }
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @ApiModel("EqualCourse - Remove")
+    public static class Remove extends EqualCourseDTO {
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private Long id;
+        @NotEmpty
+        @ApiModelProperty(required = true)
+        private Long courseId;
+    }
+}
+
