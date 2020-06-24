0b65380f385d606ce21fbfa38dab5db18ad0c4f0
heydaripoor <m.heydaripoor@gmail.com>
2020-06-24 15:59:19 +0430

    برطرف کردن مشکل sort  در چاپ ثبت نمرات و چاپ فرم مهارت

--- web/src/main/java/com/nicico/training/controller/SkillFormController.java
+++ web/src/main/java/com/nicico/training/controller/SkillFormController.java
@@ -11,7 +11,10 @@ import org.springframework.http.*;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,7 +37,7 @@ public class SkillFormController {
     }
 
 
-    @RequestMapping("/print-all/{type}")
+   // @RequestMapping("/print-all/{type}")
     public ResponseEntity<?> print(final HttpServletRequest request, @PathVariable String type) {
 
 
@@ -59,6 +62,29 @@ public class SkillFormController {
             return restTemplate.exchange(restApiUrl + "/api/skill/print-all/excel", HttpMethod.GET, entity, byte[].class);
         else if (type.equals("html"))
             return restTemplate.exchange(restApiUrl + "/api/skill/print-all/html", HttpMethod.GET, entity, byte[].class);
+        else
+            return null;
+    }
+
+    @PostMapping("/print-all/{type}")
+    public ResponseEntity<?> printWithCriteria(final HttpServletRequest request, @PathVariable String type) {
+        String token = (String) request.getParameter("token");
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("CriteriaStr", request.getParameter("CriteriaStr"));
+        map.add("_sortBy", request.getParameter("sortBy"));
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        String restApiUrl = request.getRequestURL().toString().replace(request.getServletPath(), "");
+        if (type.equals("pdf"))
+            return restTemplate.exchange(restApiUrl + "/api/skill/print-all/pdf",HttpMethod.POST, entity, byte[].class);
+        else if (type.equals("excel"))
+            return restTemplate.exchange(restApiUrl + "/api/skill/print-all/excel",HttpMethod.POST, entity, byte[].class);
+        else if (type.equals("html"))
+            return restTemplate.exchange(restApiUrl + "/api/skill/print-all/html",HttpMethod.POST, entity, byte[].class);
         else
             return null;
     }
