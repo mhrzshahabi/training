2b3baf4f18a363a5b6dd635c9878a96279dea0be
razmnoosh <razm.roya@gmail.com>
2020-07-11 12:30:18 +0430

    TMS-1137 liquibase changes

---
+++ controller/src/main/java/com/nicico/training/controller/ViewTeacherReportRestController.java
@@ -0,0 +1,44 @@
+package com.nicico.training.controller;
+
+import com.nicico.copper.common.dto.search.SearchDTO;
+import com.nicico.training.dto.ViewTeacherReportDTO;
+import com.nicico.training.service.ViewTeacherReportService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/view-teacher-report")
+public class ViewTeacherReportRestController {
+
+    private final ViewTeacherReportService viewTeacherReportService;
+    @Autowired
+    protected EntityManager entityManager;
+
+    @GetMapping(value = "/iscList")
+    public ResponseEntity<ISC<ViewTeacherReportDTO.Info>> iscList(HttpServletRequest iscRq) throws IOException {
+        int startRow = 0;
+        if (iscRq.getParameter("_startRow") != null)
+            startRow = Integer.parseInt(iscRq.getParameter("_startRow"));
+        SearchDTO.SearchRq searchRq = ISC.convertToSearchRq(iscRq);
+        if(searchRq.getCriteria() != null && searchRq.getCriteria().getCriteria() != null){
+            for (SearchDTO.CriteriaRq criterion : searchRq.getCriteria().getCriteria()) {
+                if(criterion.getValue().get(0).equals("true"))
+                    criterion.setValue(true);
+                if(criterion.getValue().get(0).equals("false"))
+                    criterion.setValue(false);
+            }
+        }
+        SearchDTO.SearchRs<ViewTeacherReportDTO.Info> searchRs = viewTeacherReportService.search(searchRq);
+        return new ResponseEntity<>(ISC.convertToIscRs(searchRs, startRow), HttpStatus.OK);
+    }
+}
---
+++ model/src/main/java/com/nicico/training/model/ViewTeacherReport.java
@@ -0,0 +1,58 @@
+package com.nicico.training.model;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.Subselect;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Entity
+@Subselect("select * from VIEW_TEACHER_REPORT")
+@DiscriminatorValue("ViewTeacherReport")
+public class ViewTeacherReport extends Auditable {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "TEACHER_ID")
+    private Long teacherId;
+    @Column(name = "PERSONALINFO_FIRST_NAME")
+    private String firstName;
+    @Column(name = "PERSONALINFO_LAST_NAME")
+    private String lastName;
+    @Column(name = "PERSONALINFO_NATIONAL_CODE")
+    private String nationalCode;
+    @Column(name = "CONTACTINFO_MOBILE")
+    private String mobile;
+    @Column(name = "CLASS_COUNTS")
+    private Integer classCounts;
+    @Column(name = "TEACHER_ENABLE_STATUS")
+    private Boolean teacherEnableStatus;
+    @Column(name = "PERSONNEL_ENABLE_STATUS")
+    private Boolean personnelStatus;
+    @Column(name = "TEACHER_CODE")
+    private String teacherCode;
+    @Column(name = "TEACHER_PERSONNEL_COD")
+    private String personnelCode;
+    @Column(name = "PERSONALINFO_EDUCATION_MAJOR")
+    private Long educationMajor;
+    @Column(name = "PERSONALINFO_EDUCATION_LEVEL")
+    private Long educationLevel;
+    @Column(name = "ADDRESS_STATE")
+    private Long state;
+    @Column(name = "ADDRESS_CITY")
+    private Long city;
+    @Column(name = "LASTCLASSTITLE")
+    private String lastClass;
+    @Column(name = "REACTIONGRADE")
+    private String lastClassGrade;
+
+}
