3d9bca05753ec82f9cd491b5caa0e6d850d842d3
haeri_am <oliya@gmail.com>
2020-08-23 11:33:11 +0430

    Training over time is changed with view.

--- service/src/main/java/com/nicico/training/iservice/ITrainingOverTimeService.java
+++
@@ -1,9 +0,0 @@
-package com.nicico.training.iservice;
-
-import com.nicico.training.dto.TrainingOverTimeDTO;
-
-import java.util.List;
-
-public interface ITrainingOverTimeService {
-    public List<TrainingOverTimeDTO.Info> getTrainingOverTimeReportList(String startDate, String endDate);
-}
