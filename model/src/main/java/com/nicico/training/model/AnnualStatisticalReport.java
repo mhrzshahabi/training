0d8d1f7a1bb3e0e5fe67b496007b85c80044193a
golrokhi <123456789>
2020-09-20 08:37:19 +0430

    اضافه کردنی متنی با عنوان " گزارش شامل کلاسهای پایان یافته و لغو شده است" درصورت کردن سطرهای تکراری درگزارش آماری سالیانه
    TMS-1616

--- model/src/main/java/com/nicico/training/model/AnnualStatisticalReport.java
+++ model/src/main/java/com/nicico/training/model/AnnualStatisticalReport.java
@@ -19,11 +19,11 @@ public class AnnualStatisticalReport {
     @EmbeddedId
     private AnnualStatisticalReportKey id;
 
-    @Column(name = "institute_id")
+    @Column(name = "institute_id", insertable = false, updatable = false)
     private Long institute_id;
     @Column(name = "institute_title_fa")
     private String institute_title_fa;
-    @Column(name = "category_id")
+    @Column(name = "category_id", insertable = false, updatable = false)
     private Long category_id;
     @Column(name = "finished_class_count")
     private Long finished_class_count;
