fbe59f94bc3f70c039e7b9712583bc1a2f7966ea
Rastegari_h <aaaa@aa>
2020-09-29 09:27:55 +0330

    competence workflow bug fixed

--- repository/src/main/java/com/nicico/training/repository/CompetenceDAO.java
+++ repository/src/main/java/com/nicico/training/repository/CompetenceDAO.java
@@ -8,7 +8,6 @@ package com.nicico.training.repository;
 import com.nicico.training.model.Competence;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
 
 public interface CompetenceDAO extends BaseDAO<Competence, Long> {
@@ -19,7 +18,7 @@ public interface CompetenceDAO extends BaseDAO<Competence, Long> {
     Optional<Competence> findTopByCodeStartsWithOrderByCodeDesc(String code);
 
     @Modifying
-    @Query(value = " update DEVTRAINING.TBL_COMPETENCE set N_WORK_FLOW_CODE = :code where ID = :competenceId ", nativeQuery = true)
+    @Query(value = " update TBL_COMPETENCE set N_WORK_FLOW_CODE = :code where ID = :competenceId ", nativeQuery = true)
     public int updateCompetenceState(Long competenceId, Integer code);
 
     @Query(value = "select NVL(MAX(CAST(SUBSTR(c_code,7) AS INTEGER))+1, 0) from TBL_COMPETENCE where c_code like :code", nativeQuery = true)
