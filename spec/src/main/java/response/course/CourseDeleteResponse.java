938055a420ffb3fd7b56f4f091284beed7d17ffb
jafari-h <oliya.470@gmail.com>
2020-10-04 14:50:19 +0330

    modify messages for remove course

---
+++ spec/src/main/java/response/course/CourseDeleteResponse.java
@@ -0,0 +1,11 @@
+package response.course;
+
+import lombok.Getter;
+import lombok.Setter;
+import response.BaseResponse;
+import response.course.dto.CourseDto;
+
+@Getter
+@Setter
+public class CourseDeleteResponse extends BaseResponse {
+}
