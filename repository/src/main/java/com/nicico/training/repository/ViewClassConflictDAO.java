d56106320c92edb9f86cd1a6b9f237b6ec74e44b
abiz <abasalt.zare@gmail.com>
2021-03-07 10:10:38 +0330

    TMS-2094

---
+++ repository/src/main/java/com/nicico/training/repository/ViewClassConflictDAO.java
@@ -0,0 +1,14 @@
+package com.nicico.training.repository;
+
+import com.nicico.training.model.ViewClassConflict;
+import com.nicico.training.model.compositeKey.ClassConflictKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface ViewClassConflictDAO extends JpaRepository<ViewClassConflict, ClassConflictKey>, JpaSpecificationExecutor<ViewClassConflict> {
+    List<ViewClassConflict> findAllByTermId(Long termId);
+
+    List<ViewClassConflict> findAllByClass1Id(Long classId);
+}
