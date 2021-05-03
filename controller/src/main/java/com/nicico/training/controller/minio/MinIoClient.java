95059a74f7214cb2bfb2eb51484887710ebad4dc
xaniar namdari <zanyarnamdari@gmail.com>
2021-05-03 14:40:54 +0430

    add minio

--- controller/src/main/java/com/nicico/training/controller/minio/MinIoClient.java
+++ controller/src/main/java/com/nicico/training/controller/minio/MinIoClient.java
@@ -2,45 +2,49 @@ package com.nicico.training.controller.minio;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-import request.evaluation.ElsEvalRequest;
-import request.exam.ElsExamRequest;
-import request.exam.ElsExtendedExamRequest;
+import org.springframework.web.multipart.MultipartFile;
+import request.minio.CreateFmsGroupReq;
 import response.BaseResponse;
-import response.evaluation.EvalListResponse;
-import response.evaluation.PdfResponse;
-import response.evaluation.dto.EvaluationDoneOnlineDto;
-import response.exam.DoneOnlineExamDto;
-import response.exam.ExamListResponse;
-
-import java.util.List;
+import response.minio.CreateFmsGroupRes;
+import response.minio.UploadFmsRes;
 
 
-@FeignClient(value = "minIoClient", url = "http://devapp01.icico.net.ir")
+@FeignClient(value = "minIoClient", url = "http://devapp01.icico.net.ir/fms/")
 public interface MinIoClient {
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/group/create")
-    BaseResponse createGroup(@RequestBody ElsEvalRequest request);
+    @RequestMapping(method = RequestMethod.POST, value = "group/create")
+    CreateFmsGroupRes createGroup(@RequestHeader("Authorization") String token,@RequestBody CreateFmsGroupReq request);
+
+    @RequestMapping(method = RequestMethod.POST,value = "{groupId}")
+    UploadFmsRes uploadFile(@RequestHeader("Authorization") String token,
+                            @RequestParam MultipartFile file,
+                            @PathVariable("groupId") String groupId);
+
+    @RequestMapping(method = RequestMethod.GET,value = "{groupId}/{key}")
+    UploadFmsRes downloadFile(@RequestHeader("Authorization") String token,
+                            @PathVariable("groupId") String groupId,
+                            @PathVariable("key") String key);
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "/group/join")
-    BaseResponse joinGroup(@RequestBody ElsEvalRequest request);
-
-    @RequestMapping(method = RequestMethod.POST, value = "/group/leave")
-    BaseResponse leaveGroup(@RequestBody ElsEvalRequest request);
-
-    @RequestMapping(method = RequestMethod.POST, value = "/{bucketName}/{groupId}")
-    BaseResponse uploadFile(@PathVariable("bucketName") String bucketName,@PathVariable("groupId") String groupId);
-
-    @RequestMapping(method = RequestMethod.GET, value = "/{bucketName}/{groupId}/{uuid}")
-    BaseResponse downloadFile(@PathVariable("bucketName") String bucketName,@PathVariable("groupId") String groupId,
-                              @PathVariable("uuid") String uuid);
-
-    @RequestMapping(method = RequestMethod.GET, value = "/info/{uuid}")
-    BaseResponse getInfo(@PathVariable("uuid") String uuid);
-
-    @RequestMapping(method = RequestMethod.POST, value = "/tag/{uuid}")
-    BaseResponse createTag(@PathVariable("uuid") String uuid);
+//    @RequestMapping(method = RequestMethod.POST, value = "/group/join")
+//    BaseResponse joinGroup(@RequestBody ElsEvalRequest request);
+//
+//    @RequestMapping(method = RequestMethod.POST, value = "/group/leave")
+//    BaseResponse leaveGroup(@RequestBody ElsEvalRequest request);
+//
+//    @RequestMapping(method = RequestMethod.POST, value = "/{bucketName}/{groupId}")
+//    BaseResponse uploadFile(@PathVariable("bucketName") String bucketName,@PathVariable("groupId") String groupId);
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/{bucketName}/{groupId}/{uuid}")
+//    BaseResponse downloadFile(@PathVariable("bucketName") String bucketName,@PathVariable("groupId") String groupId,
+//                              @PathVariable("uuid") String uuid);
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/info/{uuid}")
+//    BaseResponse getInfo(@PathVariable("uuid") String uuid);
+//
+//    @RequestMapping(method = RequestMethod.POST, value = "/tag/{uuid}")
+//    BaseResponse createTag(@PathVariable("uuid") String uuid);
 
 
 }
---
+++ spec/src/main/java/request/minio/CreateFmsGroupReq.java
@@ -0,0 +1,15 @@
+package request.minio;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+public class CreateFmsGroupReq implements Serializable {
+    private String name;
+    private boolean general;
+    private boolean encrypted;
+}
