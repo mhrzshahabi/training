ecd2bb9e26961ad273eff63ff5c804443ca5b8e3
Mahraz Shahabi <mshahabi@gmail.com>
2021-08-08 04:30:47 +0000

    TMS-2496

---
+++ spec/src/main/java/response/tclass/dto/ElsSessionUsersDto.java
@@ -0,0 +1,13 @@
+package response.tclass.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ElsSessionUsersDto {
+
+    private Long userId;
+    private String fullName;
+    private String nationalCode;
+}
