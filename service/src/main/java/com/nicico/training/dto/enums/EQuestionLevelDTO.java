0ef0b96b73e71dd01e386ffda42d672c2bd88dda
Mahraz Shahabi <mshahabi@gmail.com>
2021-08-16 05:28:51 +0000

    TMS-2537

---
+++ service/src/main/java/com/nicico/training/dto/enums/EQuestionLevelDTO.java
@@ -0,0 +1,38 @@
+package com.nicico.training.dto.enums;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nicico.training.model.enums.EQuestionLevel;
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@Getter
+@Setter
+@Accessors(chain = true)
+
+public class EQuestionLevelDTO {
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @ApiModel("EQuestionLevelSpecRs")
+    public static class EQuestionLevelSpecRs {
+        private EQuestionLevelDTO.SpecRs response = new EQuestionLevelDTO.SpecRs();
+    }
+
+
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class SpecRs {
+        private EQuestionLevel[] data = EQuestionLevel.values();
+        private Integer startRow = 0;
+        private Integer endRow = EQuestionLevel.values().length;
+        private Integer totalRows = EQuestionLevel.values().length;
+    }
+
+}
