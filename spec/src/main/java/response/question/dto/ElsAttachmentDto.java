dff88da440d624adbcd9cec5acd7e5b2c0fd1674
Mahraz Shahabi <mshahabi@gmail.com>
2021-08-23 10:55:18 +0000

    Tms 2538

--- spec/src/main/java/response/question/dto/ElsAttachmentDto.java
+++ spec/src/main/java/response/question/dto/ElsAttachmentDto.java
@@ -3,11 +3,13 @@ package response.question.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ElsAttachmentDto {
 
     @ApiModelProperty
