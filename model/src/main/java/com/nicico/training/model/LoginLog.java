4d57b0b248ca85285eea7eea69bffbbe6469a577
nima <nmoosavi@live.com>
2021-09-06 15:17:57 +0430

    Add log to LoginLog

---
+++ model/src/main/java/com/nicico/training/model/LoginLog.java
@@ -0,0 +1,53 @@
+package com.nicico.training.model;
+
+
+import com.nicico.training.model.enums.LoginState;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_login_log")
+public class LoginLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "login_log_seq")
+    @SequenceGenerator(name = "login_log_seq", sequenceName = "seq_login_log_id", allocationSize = 1)
+    @Column(name = "id", precision = 10)
+    private Long id;
+
+    @Column(name = "USER_NAME")
+    private String username;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "NATIONAL_CODE")
+    private String nationalCode;
+
+    @Column(name = "CREATE_DATE")
+    private Timestamp createDate;
+
+    @Column(name = "LOGIN_STATE")
+    @Enumerated(EnumType.STRING)
+    private LoginState state;
+
+    @Column(name = "HASH_TOKEN")
+    private String hashToken;
+
+
+    @PrePersist
+    public void setDate() {
+        this.createDate = new Timestamp(System.currentTimeMillis());
+    }
+
+}
