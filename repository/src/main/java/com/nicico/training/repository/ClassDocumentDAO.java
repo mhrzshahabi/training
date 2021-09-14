96063a38f3cba3c03f8788f88641e421fef0dda1
Mahraz Shahabi <mshahabi@gmail.com>
2021-09-14 11:07:54 +0000

    TMS-2057

--- repository/src/main/java/com/nicico/training/repository/ClassDocumentDAO.java
+++ repository/src/main/java/com/nicico/training/repository/ClassDocumentDAO.java
@@ -5,6 +5,7 @@ package com.nicico.training.repository;
 import com.nicico.training.model.ClassDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,8 @@ import java.util.List;
 public interface ClassDocumentDAO extends JpaRepository<ClassDocument, Long>, JpaSpecificationExecutor<ClassDocument> {
 
     List<ClassDocument> findClassDocumentByTclassId(Long id);
+
+    @Query(value = "select cd.C_LETTER_NUM from TBL_CLASS_DOCUMENT cd where cd.F_CLASS_ID =:classId", nativeQuery = true)
+    List<String> findAllLetterNumByClassId(Long classId);
 
 }
