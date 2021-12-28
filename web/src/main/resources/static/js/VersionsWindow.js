6c7c6a02c026c6d0bca20136dd7e2982df0f1afb
Mohammad Tavasoli <mtavasoli@gmail.com>
2021-12-28 10:07:46 +0330

    TMS-2860 add version description
    added version description in the header
    changed question bank list sort type
    to be descending

---
+++ web/src/main/resources/static/js/VersionsWindow.js
@@ -0,0 +1,17 @@
+isc.defineClass("VersionsWindow", isc.Window);
+isc.VersionsWindow.addProperties({
+    width: "500", height: "400", isModal: true,
+    autoCenter: true,
+    autoDraw: true,
+    title:"امکانات نسخه جدید",
+    initWidget: function () {
+        this.Super("initWidget", arguments);
+        this.pageLoader =isc.HTMLFlow.create({
+                autoDraw:false,
+                width: "100%",height:"100%",
+                // contentsURL: "static/js/versions.html"
+                contentsURL: "js/versions.html"
+             })
+        this.addItems(this.pageLoader)
+    }
+});
