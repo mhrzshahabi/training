9036cc0d403580ac35658ecd013fa439f41999fb
Mahraz Shahabi <mshahabi@gmail.com>
2022-01-04 07:55:08 +0000

    TMS-2850

--- service/src/main/java/com/nicico/training/service/NeedsAssessmentReportsService.java
+++ service/src/main/java/com/nicico/training/service/NeedsAssessmentReportsService.java
@@ -55,7 +55,6 @@ public class NeedsAssessmentReportsService implements INeedsAssessmentReportsSer
     private final ParameterDAO parameterDAO;
     private final ITclassService tClassService;
     private final ICourseService courseService;
-    private final ISkillService skillService;
     private final NeedAssessmentGroupResultDAO needAssessmentGroupResultDAO;
     protected EntityManager entityManager;
     private final ParameterValueDAO parameterValueDAO;
@@ -531,13 +530,14 @@ public class NeedsAssessmentReportsService implements INeedsAssessmentReportsSer
         List<NeedAssessmentReportUserDTO> reportUserDTOS = new ArrayList<>();
         NeedAssessmentReportUserObj needAssessmentReportUserObj = new NeedAssessmentReportUserObj();
         PersonnelDTO.PersonalityInfo personalityInfo = personnelService.getByNationalCode(nationalCode);
-        if (personalityInfo!=null && personalityInfo.getPostCode()!=null && personalityInfo.getPersonnelNo()!=null){
+        if (personalityInfo!=null && personalityInfo.getPostCode()!=null && personalityInfo.getPersonnelNo()!=null) {
             List<NeedsAssessmentReportsDTO.ReportInfo> needsAssessmentReportList = getCourseListForBpms(personalityInfo.getPostCode(), "Post", nationalCode, personalityInfo.getPersonnelNo());
             Set<Long> assessmentsType = needsAssessmentReportList.stream().map(NeedsAssessmentReportsDTO.ReportInfo::getNeedsAssessmentPriorityId).collect(Collectors.toSet());
             for (Long aLong : assessmentsType) {
                 NeedAssessmentReportUserDTO needAssessmentReportUserDTO = new NeedAssessmentReportUserDTO();
                 String title = allParameter.stream().filter(parameterValue -> parameterValue.getId().equals(aLong)).collect(Collectors.toList()).get(0).getTitle();
                 needAssessmentReportUserDTO.setAssessment(title);
+                needAssessmentReportUserDTO.setAssessmentOrdinary(getNeedAssessmentOrdinary(aLong));
                 List<NeedAssessmentReportUserDTO.CompetenceInfo> competenceInfoList = new ArrayList<>();
                 needsAssessmentReportList.stream().filter(reportInfo -> reportInfo.getNeedsAssessmentPriorityId().equals(aLong)).collect(Collectors.toList()).forEach(reportInfo -> {
                     NeedAssessmentReportUserDTO.CompetenceInfo competenceInfo = new NeedAssessmentReportUserDTO.CompetenceInfo();
@@ -554,18 +554,57 @@ public class NeedsAssessmentReportsService implements INeedsAssessmentReportsSer
                 });
                 needAssessmentReportUserDTO.setCompetenceInfoList(competenceInfoList);
                 needAssessmentReportUserDTO.setNeedAssessmentCount(competenceInfoList.size());
-                needAssessmentReportUserDTO.setNeedAssessmentDurationCount(competenceInfoList.stream().mapToDouble(NeedAssessmentReportUserDTO.CompetenceInfo::getCourseDuration).sum());
+                needAssessmentReportUserDTO.setNeedAssessmentDurationCount(getWholeDuration(competenceInfoList));
                 needAssessmentReportUserDTO.setNeedAssessmentPassCount(competenceInfoList.stream().filter(competenceInfo -> competenceInfo.getCourseState().equalsIgnoreCase(PASS)).count());
-                needAssessmentReportUserDTO.setNeedAssessmentDurationPass(competenceInfoList.stream().filter(competenceInfo -> competenceInfo.getCourseState().equalsIgnoreCase(PASS)).mapToDouble(NeedAssessmentReportUserDTO.CompetenceInfo::getCourseDuration).sum());
+                needAssessmentReportUserDTO.setNeedAssessmentDurationPass(getWholePassedDuration(competenceInfoList));
                 reportUserDTOS.add(needAssessmentReportUserDTO);
-        }
-            needAssessmentReportUserObj.setReportUserDTOS(reportUserDTOS);
+            }
+
+            List<NeedAssessmentReportUserDTO> reportUserDTOList = reportUserDTOS.stream().sorted(Comparator.comparing(NeedAssessmentReportUserDTO::getAssessmentOrdinary)).collect(Collectors.toList());
+            reportUserDTOList.forEach(item -> item.setAssessmentOrdinary(null));
+            needAssessmentReportUserObj.setReportUserDTOS(reportUserDTOList);
             needAssessmentReportUserObj.setCode(personalityInfo.getPostCode());
             needAssessmentReportUserObj.setPostTitle(personalityInfo.getPostTitle());
         }
         return needAssessmentReportUserObj;
     }
 
+    private Integer getNeedAssessmentOrdinary(Long needAssessmentId) {
+        if (needAssessmentId == 574) {
+            return 1;
+        } else if (needAssessmentId == 111) {
+            return 2;
+        } else if (needAssessmentId == 112) {
+            return 3;
+        } else if (needAssessmentId == 113) {
+            return 4;
+        }
+        return 0;
+    }
+
+    private Double getWholeDuration(List<NeedAssessmentReportUserDTO.CompetenceInfo> competenceInfoList) {
+        Double sum = 0d;
+        List<String> courseCodes = new ArrayList<>();
+        for (NeedAssessmentReportUserDTO.CompetenceInfo competenceInfo : competenceInfoList) {
+            if (!courseCodes.contains(competenceInfo.getCourseCode())) {
+                courseCodes.add(competenceInfo.getCourseCode());
+                sum += competenceInfo.getCourseDuration();
+            }
+        }
+        return sum;
+    }
+
+    private Double getWholePassedDuration(List<NeedAssessmentReportUserDTO.CompetenceInfo> competenceInfoList) {
+        Double passedSum = 0d;
+        List<String> courseCodes = new ArrayList<>();
+        for (NeedAssessmentReportUserDTO.CompetenceInfo competenceInfo : competenceInfoList) {
+            if (!courseCodes.contains(competenceInfo.getCourseCode()) && competenceInfo.getCourseState().equalsIgnoreCase(PASS)) {
+                courseCodes.add(competenceInfo.getCourseCode());
+                passedSum += competenceInfo.getCourseDuration();
+            }
+        }
+        return passedSum;
+    }
 
     @Transactional(readOnly = true)
     @Override
