be6033b3a5eba4495c32404de9285a98ddd11c42
Mohammad Tavasoli <mtavasoli@gmail.com>
2022-01-19 12:15:46 +0330

    TMS-2893 addin AttendanceAudit for attendance history
    added AttendanceAudit for showing attendance history of a student
    added a button in attendance component in classes to show attendance
    changes history

---
+++ service/src/main/java/com/nicico/training/service/AttendanceAuditService.java
@@ -0,0 +1,20 @@
+package com.nicico.training.service;
+
+import com.nicico.training.iservice.IAttendanceAuditService;
+import com.nicico.training.model.AttendanceAudit;
+import com.nicico.training.repository.AttendanceAuditDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AttendanceAuditService implements IAttendanceAuditService {
+    private final AttendanceAuditDao attendanceAuditDao;
+
+    @Override
+    public List<AttendanceAudit> getChangeList(Long attendanceId) {
+        return attendanceAuditDao.findAllByIdOrderByCreatedDate(attendanceId);
+    }
+}
