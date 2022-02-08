6b0a5770b5c31e64e393c32a92cbea8ad6e10c95
Mohammad Tavasoli <mtavasoli@gmail.com>
2022-02-08 11:16:16 +0330

    TMS-2956 Evaluation Answered Questions Details backend
    added dto and changed related controller and service

---
+++ spec/src/main/java/dto/evaluuation/EvaluationAnsweredQuestionsDetailsDTO.java
@@ -0,0 +1,18 @@
+package dto.evaluuation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EvaluationAnsweredQuestionsDetailsDTO {
+    private String classCode;
+    private String classTitle;
+    private String answerTitle;
+    private String firstName;
+    private String lastName;
+    private String nationalCode;
+    private String complexTitle;
+    private String questionTitle;
+
+}
