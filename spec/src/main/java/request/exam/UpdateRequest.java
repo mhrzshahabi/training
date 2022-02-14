714c20afbf248af5e46ec1a46ae9eba90c50eb68
xnamdari <you@example.com>
2022-02-14 11:21:17 +0330

    fix pagination

--- spec/src/main/java/request/exam/UpdateRequest.java
+++ spec/src/main/java/request/exam/UpdateRequest.java
@@ -3,11 +3,12 @@ package request.exam;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Setter
 @Getter
-public class UpdateRequest {
+public class UpdateRequest implements Serializable {
     private Long sourceExamId;
     private String modifiedBy;
     private List<UpdatedResultDto> results;
