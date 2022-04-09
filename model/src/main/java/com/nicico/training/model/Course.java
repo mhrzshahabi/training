3862542a40c0e135cfc1d43ce8c9a43b30ccc603
Mohammad Tavasoli <mtavasoli@gmail.com>
2022-04-09 11:50:14 +0430

    TMS-3009 course audit
    added audit to course
    created new entities
    added liquibase changes

--- model/src/main/java/com/nicico/training/model/Course.java
+++ model/src/main/java/com/nicico/training/model/Course.java
@@ -6,6 +6,10 @@ import com.nicico.training.model.enums.ETechnicalType;
 import com.nicico.training.model.enums.ETheoType;
 import lombok.*;
 import lombok.experimental.Accessors;
+import org.hibernate.envers.AuditOverride;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -19,6 +23,8 @@ import java.util.Set;
 @EqualsAndHashCode(of = {"id"}, callSuper = false)
 @Entity
 @Table(name = "tbl_course")
+@Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+@AuditOverride(forClass =Auditable.class )
 public class Course extends Auditable {
 
     @Id
@@ -59,6 +65,7 @@ public class Course extends Auditable {
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @NotAudited
     private Category category;
 
     @Column(name = "category_id", insertable = false, updatable = false)
@@ -66,30 +73,36 @@ public class Course extends Auditable {
 
     @ManyToOne
     @JoinColumn(name = "subcategory_id")
+    @NotAudited
     private Subcategory subCategory;
 
     @Column(name = "subcategory_id", insertable = false, updatable = false)
     private Long subCategoryId;
 
     @OneToMany(mappedBy = "course")
+    @NotAudited
     private Set<Skill> skillSet;
 
     @OneToMany(mappedBy = "courseMainObjective", cascade = {CascadeType.MERGE})
+    @NotAudited
     private Set<Skill> skillMainObjectiveSet;
 
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    @NotAudited
     private Set<Tclass> tclassSet;
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "tbl_course_goal",
             joinColumns = {@JoinColumn(name = "f_course_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "f_goal_id", referencedColumnName = "id")})
+    @NotAudited
     private List<Goal> goalSet;
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "tbl_pre_course", uniqueConstraints = {@UniqueConstraint(columnNames = {"f_course_id", "f_pre_course_id"})},
             joinColumns = {@JoinColumn(name = "f_course_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "f_pre_course_id", referencedColumnName = "id")})
+    @NotAudited
     private List<Course> preCourseList;
 
     @Column(name = "e_run_type")
@@ -114,17 +127,23 @@ public class Course extends Auditable {
     private Integer startEvaluation;
 
     @Column(name = "c_need_text")
+    @NotAudited
     private String needText;
 
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @NotAudited
     private List<EqualCourse> equalCourses;
 
     @Column(name = "b_has_goal")
+    @NotAudited
     private Boolean hasGoal;
 
     @Column(name = "c_workflow_status")
+    @NotAudited
     private String workflowStatus;
+
     @Column(name = "c_workflow_status_code")
+    @NotAudited
     private Integer workflowStatusCode;
 
     @PreRemove
@@ -133,5 +152,6 @@ public class Course extends Auditable {
     }
 
     @Column(name = "n_has_skill")
+    @NotAudited
     private Boolean hasSkill;
 }
---
+++ model/src/main/java/com/nicico/training/model/compositeKey/AuditCourseId.java
@@ -0,0 +1,24 @@
+package com.nicico.training.model.compositeKey;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id", "rev"}, callSuper = false)
+@Embeddable
+public class AuditCourseId implements Serializable {
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "rev")
+    private Long rev;
+}
