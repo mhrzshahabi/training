f244846eefbd8d7b9e384f9bc2583b340b2f940d
xnamdari <you@example.com>
2022-04-09 10:51:39 +0430

    fix evaluation jasper

--- controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
+++ controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
@@ -370,12 +370,12 @@ public class EvaluationAnalysisRestController {
                 int index = 1;
                 if (list.size()>10){
                     int  totalPage = (list.size() / 10)+1;
-                    List<List<ViewEvaluationStaticalReportDTO.Info>> subLists=  GetSubList(list,totalPage);
-                    for (int m=0 ; m<subLists.size();m++){
-                        List<ViewEvaluationStaticalReportDTO.Info> subV2=subLists.get(m);
+//                    List<List<ViewEvaluationStaticalReportDTO.Info>> subLists=  GetSubList(list,totalPage);
+                    for (int m=0 ; m<list.size();m++){
+//                        List<ViewEvaluationStaticalReportDTO.Info> subV2=list.get(m);
 
-                        for (int z=0 ; z<subV2.size();z++) {
-                            ViewEvaluationStaticalReportDTO.Info info=subV2.get(z);
+                        for (int z=0 ; z<list.size();z++) {
+                            ViewEvaluationStaticalReportDTO.Info info=list.get(z);
                             System.out.println("zaza"+info.getCourseTitleFa());
                             chartData.add(new ChartData("زازا" , z+1 + "" ,
 //                            chartData.add(new ChartData(PersianCharachtersUnicode.bidiReorder(info.getCourseTitleFa()) + "/" + info.getTclassCode(), z+1 + "" ,
