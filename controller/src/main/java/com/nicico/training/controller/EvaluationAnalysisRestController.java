bcf5c644f6a201765ae2927e9b22f965db7ae3dc
xnamdari <you@example.com>
2022-04-09 13:33:57 +0430

    fix evaluation jasper

--- controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
+++ controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
@@ -372,7 +372,7 @@ public class EvaluationAnalysisRestController {
                     int  totalPage = (list.size() / 10)+1;
                     List<List<ViewEvaluationStaticalReportDTO.Info>> subLists=  GetSubList(list,totalPage);
 
-                    for (int m=0 ; m<subLists.size()-1;m++){
+                    for (int m=0 ; m<subLists.size()-2;m++){
                         List<ViewEvaluationStaticalReportDTO.Info> subV2=subLists.get(m);
 
                         for (int z=0 ; z<subV2.size();z++) {
