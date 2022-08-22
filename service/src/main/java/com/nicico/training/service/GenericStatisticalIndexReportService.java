85de28cfa17952c7d66642975f5dd2a8e4286472
t.kasehpoor <kasehpoor.k@gmail.com>
2022-08-22 10:10:42 +0430

    add reportPer capita cost of employee training.

--- service/src/main/java/com/nicico/training/service/GenericStatisticalIndexReportService.java
+++ service/src/main/java/com/nicico/training/service/GenericStatisticalIndexReportService.java
@@ -108,6 +108,7 @@ public class GenericStatisticalIndexReportService implements IGenericStatistical
             case "report48"-> genericStatisticalIndexReportDAO.coursesDeterminedEvaluationMethod(fromDate, toDate, complex, complexNull, assistant, assistantNull, affairs, affairsNull);
             case "report49"-> genericStatisticalIndexReportDAO.coursesTargetDeterminedEvaluationMethod(fromDate, toDate, complex, complexNull, assistant, assistantNull, affairs, affairsNull);
             case "report50"-> genericStatisticalIndexReportDAO.scheduledTraining(fromDate, toDate, complex, complexNull, assistant, assistantNull, affairs, affairsNull);
+            case "report60"-> genericStatisticalIndexReportDAO.perCapitaCostOfEmployeeTraining(fromDate, toDate, complex, complexNull, assistant, assistantNull, affairs, affairsNull);
                 default -> null;
         };
 
