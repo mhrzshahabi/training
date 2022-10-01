d4eadaa47055742adeecf1e020e8a3ad51c74e29
xnamdari <you@example.com>
2022-10-01 15:53:29 +0330

    change version

--- controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
+++ controller/src/main/java/com/nicico/training/controller/EvaluationAnalysisRestController.java
@@ -471,7 +471,7 @@ public class EvaluationAnalysisRestController {
                         for (int z=0 ; z<subV2.size();z++) {
                             ViewEvaluationStaticalReportDTO.Info info=subV2.get(z);
 
-                            chartData.add(new ChartData(PersianCharachtersUnicode.bidiReorder(info.getCourseTitleFa()) + "/" + info.getTclassCode()+ "("+info.getPercentOfReaction()+"% )" , z+1 + "" ,
+                            chartData.add(new ChartData(PersianCharachtersUnicode.bidiReorder("زز") + "/" + info.getTclassCode()+ "("+info.getPercentOfReaction()+"% )" , z+1 + "" ,
                                     Double.parseDouble(df.format(Double.parseDouble(info.getEvaluationReactionGrade()))), catCount + ". واحد " + category.getTitleFa()+" بخش "+" ( "+(m+1)+" ) ",
                                     Double.parseDouble(minFerGrade.getValue())));
 
@@ -491,7 +491,7 @@ public class EvaluationAnalysisRestController {
 
                 }else {
                     for (ViewEvaluationStaticalReportDTO.Info info : list) {
-                        chartData.add(new ChartData(PersianCharachtersUnicode.bidiReorder(info.getCourseTitleFa()) + "/" + info.getTclassCode()+ "("+info.getPercentOfReaction()+"% )" , index + "",
+                        chartData.add(new ChartData(PersianCharachtersUnicode.bidiReorder("زز") + "/" + info.getTclassCode()+ "("+info.getPercentOfReaction()+"% )" , index + "",
                                 Double.parseDouble(df.format(Double.parseDouble(info.getEvaluationReactionGrade()))), catCount + ". واحد " + category.getTitleFa(),
                                 Double.parseDouble(minFerGrade.getValue())));
 
