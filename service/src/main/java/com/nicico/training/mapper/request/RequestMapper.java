f702fb82bf24f8a4fd26f9f45e631d0a393a664c
shahabi <mshahabi>
2022-11-01 15:07:36 +0330

    TMS-3515 change response API

--- service/src/main/java/com/nicico/training/mapper/request/RequestMapper.java
+++ service/src/main/java/com/nicico/training/mapper/request/RequestMapper.java
@@ -2,31 +2,37 @@ package com.nicico.training.mapper.request;
 
 import com.nicico.training.dto.RequestReqVM;
 import com.nicico.training.dto.RequestResVM;
+import com.nicico.training.iservice.ITclassService;
 import com.nicico.training.mapper.fms.AttachmentMapper;
 import com.nicico.training.model.Request;
 import com.nicico.training.model.enums.EnumsConverter;
 import com.nicico.training.model.enums.UserRequestType;
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN,uses = {AttachmentMapper.class})
-public interface RequestMapper {
+public abstract class RequestMapper {
+
+    @Autowired
+    protected ITclassService tClassService;
 
     @Mappings({
             @Mapping(target = "requestAttachmentDtos",source = "requestAttachments"),
             @Mapping(target = "responseAttachmentDtos",source = "responseAttachments"),
-            @Mapping(target = "userRequestTypeTitle", source = "type", qualifiedByName = "toUserRequestTypeTitle")
+            @Mapping(target = "userRequestTypeTitle", source = "type", qualifiedByName = "toUserRequestTypeTitle"),
+            @Mapping(target = "classTitle", source = "classId", qualifiedByName = "toClassTitle")
     })
-    RequestResVM mapEntityToRes(Request request);
+    public abstract RequestResVM mapEntityToRes(Request request);
 
-    List<RequestResVM> mapListEntityToRes(List<Request> requests);
+    public abstract List<RequestResVM> mapListEntityToRes(List<Request> requests);
 
     @Mapping(target = "type", source = "userRequestTypeId", qualifiedByName = "toUserRequestType")
-    Request mapReqToEntity(RequestReqVM requestReqVM);
+    public abstract Request mapReqToEntity(RequestReqVM requestReqVM);
 
     @Named("toUserRequestType")
-    default UserRequestType toUserRequestType(Long userRequestTypeId) {
+    protected UserRequestType toUserRequestType(Long userRequestTypeId) {
         if (userRequestTypeId != null) {
             EnumsConverter.UserRequestTypeConverter userRequestTypeConverter = new EnumsConverter.UserRequestTypeConverter();
             return userRequestTypeConverter.convertToEntityAttribute(userRequestTypeId.intValue());
@@ -34,9 +40,16 @@ public interface RequestMapper {
     }
 
     @Named("toUserRequestTypeTitle")
-    default String toUserRequestTypeTitle(UserRequestType type) {
+    protected String toUserRequestTypeTitle(UserRequestType type) {
         if (type != null)
         return type.getTitleFa();
+        else return "";
+    }
+
+    @Named("toClassTitle")
+    protected String toClassTitle(Long classId) {
+        if (classId != null)
+            return tClassService.get(classId).getTitleClass();
         else return "";
     }
 
