090e602d90ecbcbc182d830667896ee43c1d810a
t.kasehpoor <kasehpoor.k@gmail.com>
2022-12-19 12:56:32 +0330

    TMS-3706-post-grade-group-excel.

--- web/src/main/java/com/nicico/training/controller/ExportToFileController.java
+++ web/src/main/java/com/nicico/training/controller/ExportToFileController.java
@@ -122,6 +122,7 @@ public class ExportToFileController {
     @Autowired
     protected EntityManager entityManager;
 
+    private IBaseService baseService;
 
     @PostMapping(value = {"/exportExcelFromClient"})
     public void exportExcelFromClient(final HttpServletResponse response,
@@ -524,7 +525,7 @@ public class ExportToFileController {
                 Long objectId = ((Integer) NeedsAssessmentParams.get("objectId")[0]).longValue();
                 String objectType = (String) NeedsAssessmentParams.get("objectType")[0];
                 Long personnelId = NeedsAssessmentParams.get("personnelId") == null ? null : ((Integer) NeedsAssessmentParams.get("personnelId")[0]).longValue();
-                generalList = (List<Object>) ((Object) needsAssessmentReportsService.search(searchRq, objectId, objectType, personnelId).getList());
+                generalList = (List<Object>) ((Object) baseService.excelSearch(searchRq).getList());
                 break;
 
             case "View_Post":
@@ -541,7 +542,7 @@ public class ExportToFileController {
 
             case "Post_Grade_Without_Permission":
                 BaseService.setCriteriaToNotSearchDeleted(searchRq);
-                generalList = (List<Object>) ((Object) postGradeService.searchWithoutPermission(searchRq).getList());
+                generalList = (List<Object>) ((Object) baseService.excelSearch(searchRq).getList());
                 break;
 
             case "View_Job_Group":
@@ -593,7 +594,7 @@ public class ExportToFileController {
                 break;
 
             case "View_Post_Grade_Group":
-                generalList = (List<Object>) ((Object) viewPostGradeGroupService.search(searchRq).getList());
+                generalList = (List<Object>) ((Object) viewPostGradeGroupService.excelSearch(searchRq).getList());
                 break;
 
             case "Post_Grade_Group_Personnel":
@@ -622,7 +623,7 @@ public class ExportToFileController {
                 coustomSearchRq2 = ISC.convertToSearchRq(req, postGradeGroupPersonnelPostList.getList().stream().map(PostDTO.TupleInfo::getId).collect(Collectors.toList()), "postId", EOperator.inSet);
                 coustomSearchRq2.getCriteria().getCriteria().add(makeNewCriteria("deleted", 0, EOperator.equals, null));
                 coustomSearchRq2.getCriteria().getCriteria().addAll(searchRq.getCriteria().getCriteria());
-                generalList = (List<Object>) ((Object) personnelService.search(coustomSearchRq2).getList());
+                generalList = (List<Object>) ((Object) baseService.excelSearch(searchRq).getList());
                 break;
 
             case "Post_Grade_Group_Post":
@@ -638,7 +639,7 @@ public class ExportToFileController {
 
                 searchRq.getCriteria().getCriteria().add(makeNewCriteria("postGrade", postGradePosts.stream().filter(pg -> pg.getDeleted() == null).map(PostGradeDTO.Info::getId).collect(Collectors.toList()), EOperator.inSet, null));
                 BaseService.setCriteriaToNotSearchDeleted(searchRq);
-                generalList = (List<Object>) postService.searchWithoutPermission(searchRq, p -> modelMapper.map(p, PostDTO.Info.class)).getList();
+                generalList = (List<Object>) ((Object) baseService.excelSearch(searchRq).getList());
                 break;
 
             case "View_Post_Group":
