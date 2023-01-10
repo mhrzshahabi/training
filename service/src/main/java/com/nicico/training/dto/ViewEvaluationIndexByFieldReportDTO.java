584692f4b35a76bbfa45fa6c8c63fe62543fcca1
Hamid Montazeri <montazeri68@gmail.com>
2023-01-10 10:29:17 +0330

    added some criteria to evaluation indexes by field report

--- service/src/main/java/com/nicico/training/dto/ViewEvaluationIndexByFieldReportDTO.java
+++ service/src/main/java/com/nicico/training/dto/ViewEvaluationIndexByFieldReportDTO.java
@@ -1,13 +1,12 @@
 package com.nicico.training.dto;
 
 import io.swagger.annotations.ApiModel;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -22,17 +21,32 @@ public class ViewEvaluationIndexByFieldReportDTO implements Serializable {
         private Long id;
         private String courseCode;
         private String classCode;
-        private String teacherFirstName;
-        private String teacherLastName;
-        private String teacherNationalCode;
-        private String evaluationAffairs;
+        private String classStartDate;
+        private String classEndDate;
+        private String studentFirstName;
+        private String studentLastName;
+        private String studentNationalCode;
+        private String studentComplex;
+        private String studentAssistant;
+        private String studentAffairs;
+        private String studentSection;
+        private String studentUnit;
         private String postTitle;
         private String postCode;
         private String personnelNo2;
         private String studentAcceptanceStatus;
-        private String score;
-        private BigDecimal evaluationId;
-        private BigDecimal evaluationAverage;
+        private Double score;
+        private Long evaluationId;
+        private Double evaluationAverage;
+        private Long evaluationFieldId;
         private String evaluationField;
+        private Double evaluationFieldAverage;
+        @Getter(AccessLevel.NONE)
+        private String fullName;
+
+        public String getFullName() {
+            return (studentFirstName + " " + studentLastName).compareTo("null null") == 0 ? null : studentFirstName + " " + studentLastName;
+        }
+
     }
 }
