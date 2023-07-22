8a3d9d8cd4ec975ce17624f4706ffe8804577afd
xnamdari <you@example.com>
2023-07-22 14:41:56 +0330

    add new view in student

--- controller/src/main/java/com/nicico/training/controller/ElsRestController.java
+++ controller/src/main/java/com/nicico/training/controller/ElsRestController.java
@@ -3377,9 +3377,9 @@ public class ElsRestController {
 
 
     @GetMapping("/student/certification/file")
-    public byte[] getCertificationFile(@RequestParam String Authorization, HttpServletResponse response, @RequestParam String nationalCode, @RequestParam Long classId) throws JRException, SQLException, IOException, ParseException {
-       log.info("header: {}",  Authorization);
-        return tclassService.getCertificationFile(nationalCode, classId, Authorization, response);
+    public byte[] getCertificationFile(@RequestHeader(name = "X-Auth-Token") String header, HttpServletResponse response, @RequestParam String nationalCode, @RequestParam Long classId) throws JRException, SQLException, IOException, ParseException {
+       log.info("header: {}",  header);
+        return tclassService.getCertificationFile(nationalCode, classId, header, response);
     }
 
     @GetMapping("/student/certification/qr-code/file/{nationalCode}/{classId}")
